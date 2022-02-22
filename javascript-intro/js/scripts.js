e6cf380f6f2051d574bf8eb8378ac19f1819319c
etullo <97974216+etullo@users.noreply.github.com>
2022-02-22 14:42:36 -0700

    javascript demo and font change to charlies

---
+++ javascript-intro/js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    /*We're finding the nav element and hiding it here*/
+   $('nav').hide();
+    
+   //set-up our click function
+   $('.menu-button').click(function(){
+       $('nav').slideToggle();
+   });
+});
